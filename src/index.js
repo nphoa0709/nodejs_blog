761df8489b5ec1f72784f9198b243f35d23300ea
nphoa0709 <nguyenphuonghoa0709@gmail.com>
2023-06-26 11:03:43 +0700

    use bootstrap

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const morgan = require('morgan')
+const { engine } = require ('express-handlebars');
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+//static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//template engine
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+//console.log(__dirname);
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
